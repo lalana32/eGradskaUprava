7131a24a19b248f08386d3d3bb348cc5f917c528
MkCs133 <goranmilanovic2001.gm@gmail.com>
2024-08-30 17:04:19 +0200

    SignIn & SingUp

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -4,13 +4,17 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './css/style.css';
 import './css/satoshi.css';
-import 'jsvectormap/dist/css/jsvectormap.css';
+// import 'jsvectormap/dist/css/jsvectormap.css';
 import 'flatpickr/dist/flatpickr.min.css';
+import { Provider } from 'react-redux';
+import { store } from './store/configureStore';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>
+  </Provider>,
 );
